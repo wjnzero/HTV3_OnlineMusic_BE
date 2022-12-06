8396d4a012cf1497c93e63664609529da96ce4f0
wjnzero <chuckieakenzzz@gmai.com>
2022-12-06 14:43:17 +0700

    upload

---
+++ src/main/java/com/htv3/htv3onlinemusic/Htv3OnlineMusicApplication.java
@@ -0,0 +1,13 @@
+package com.htv3.htv3onlinemusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Htv3OnlineMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Htv3OnlineMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/htv3/htv3onlinemusic/Htv3OnlineMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htv3.htv3onlinemusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Htv3OnlineMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
