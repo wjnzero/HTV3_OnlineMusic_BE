67373284e26bdfd5db2ac57e8b59573b8f22212a
NguyenThanhVinh2711 <vinh99657@gmail.com>
2022-12-06 15:16:03 +0700

    model

---
+++ src/main/java/com/htv3/htv3onlinemusic/model/ProfileUser.java
@@ -0,0 +1,7 @@
+package com.htv3.htv3onlinemusic.model;
+
+
+
+public class ProfileUser {
+
+}
---
+++ src/main/java/com/htv3/htv3onlinemusic/model/User.java
@@ -0,0 +1,26 @@
+package com.htv3.htv3onlinemusic.model;
+
+
+import jdk.nashorn.internal.objects.annotations.Constructor;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String userName;
+    private String password;
+    private String phoneNumber;
+}
