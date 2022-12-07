41595f5f162cd93b2f19a84a1855a5a4c33cc65e
wjnzero <chuckieakenzzz@gmai.com>
2022-12-07 12:02:21 +0700

    done login & register API

---
+++ src/main/java/com/htv3/htv3onlinemusic/service/role/IRoleService.java
@@ -0,0 +1,7 @@
+package com.htv3.htv3onlinemusic.service.role;
+
+import com.htv3.htv3onlinemusic.model.Role;
+import com.htv3.htv3onlinemusic.service.GenericService;
+
+public interface IRoleService extends GenericService<Role> {
+}
