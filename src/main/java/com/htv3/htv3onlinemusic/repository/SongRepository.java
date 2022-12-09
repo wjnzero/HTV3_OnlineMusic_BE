1176377e43e6c34e95ab868e08182a6946a7724c
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-12-09 15:38:40 +0700

    fix

--- src/main/java/com/htv3/htv3onlinemusic/repository/SongRepository.java
+++ src/main/java/com/htv3/htv3onlinemusic/repository/SongRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SongRepository extends PagingAndSortingRepository<Song,Long> {
-    @Query(value="select * from song s where s.name like % name % ", nativeQuery=true)
+    @Query(value="select * from song s where s.name like %name% ", nativeQuery=true)
     Iterable<Song> findByNameContaining(String name);
 }
