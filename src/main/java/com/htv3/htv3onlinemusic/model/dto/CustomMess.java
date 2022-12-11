beb17b2d8e6980946d699c99d5a51e562f90a151
wjnzero <chuckieakenzzz@gmai.com>
2022-12-11 17:09:34 +0700

    done

---
+++ src/main/java/com/htv3/htv3onlinemusic/model/dto/CustomMess.java
@@ -0,0 +1,20 @@
+package com.htv3.htv3onlinemusic.model.dto;
+
+public class CustomMess {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public CustomMess() {
+    }
+
+    public CustomMess(String message) {
+        this.message = message;
+    }
+}
