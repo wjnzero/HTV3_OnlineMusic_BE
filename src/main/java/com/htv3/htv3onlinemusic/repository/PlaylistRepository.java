c745aaa66fce87feaed33c951aec28c9470b7e2f
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-12-14 17:54:11 +0700

    14

--- src/main/java/com/htv3/htv3onlinemusic/repository/PlaylistRepository.java
+++ src/main/java/com/htv3/htv3onlinemusic/repository/PlaylistRepository.java
@@ -1,9 +1,7 @@
 package com.htv3.htv3onlinemusic.repository;
 
 import com.htv3.htv3onlinemusic.model.PlayList;
-import com.htv3.htv3onlinemusic.model.Song;
 import com.htv3.htv3onlinemusic.model.dto.IPlaylist;
-import com.htv3.htv3onlinemusic.model.dto.ISong;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
