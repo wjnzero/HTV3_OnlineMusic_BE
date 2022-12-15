40c1b294d0b99ae50e45139433af7265de953e10
wjnzero <chuckieakenzzz@gmai.com>
2022-12-15 14:38:28 +0700

    done api get all comment of song

---
+++ src/main/java/com/htv3/htv3onlinemusic/service/comment/ICommentService.java
@@ -0,0 +1,8 @@
+package com.htv3.htv3onlinemusic.service.comment;
+
+import com.htv3.htv3onlinemusic.model.Comment;
+import com.htv3.htv3onlinemusic.service.GenericService;
+
+public interface ICommentService extends GenericService<Comment> {
+    Iterable<Comment> getCommentBySongId(Long id);
+}
